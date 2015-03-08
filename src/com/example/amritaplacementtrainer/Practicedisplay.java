90a41f448ec77ad769c3902db6f8d7c3c412359d
sundarkid <sundareswaran25@gmail.com>
2015-03-09 01:54:45 +0530

    Completed Practise test for TECHNICAL.

--- src/com/example/amritaplacementtrainer/Practicedisplay.java
+++ src/com/example/amritaplacementtrainer/Practicedisplay.java
@@ -2,9 +2,12 @@ package com.example.amritaplacementtrainer;
 
 
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
@@ -12,23 +15,41 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.amritaplacementtrainer.dataClass.Question;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Practicedisplay extends Activity {
-	int i;
+	int i = 1;
 	TextView tv1;
 	Button btnNext;
 	RadioGroup rg;
 	RadioButton rb0,rb1,rb2,rb3;
-	int flag=0;
-	String questions[]={"A train running at the speed of 60 km/hr crosses a pole in 9 seconds. What is the length of the train?",
-	"A train 125 m long passes a man, running at 5 km/hr in the same direction in which the train is going, in 10 seconds. The speed of the train is:",
-	"The length of the bridge, which a train 130 metres long and travelling at 45 km/hr can cross in 30 seconds, is:",
-	"Two trains running in opposite directions cross a man standing on the platform in 27 seconds and 17 seconds respectively and they cross each other in 23 seconds. The ratio of their speeds is",
-	"A train passes a station platform in 36 seconds and a man standing on the platform in 20 seconds. If the speed of the train is 54 km/hr, what is the length of the platform?"};
-	String opt[]={"120 metres","180 metres","324 metres","150 metres","45 km/hr","50 km/hr","54 km/hr","55 km/hr","200 m","225 m","245 m","250 m","1 : 3","3 : 2","3 : 4","None of these","120 m","240 m","300 m","None of these"};
+	int flag=1;
+    Question ques[];
+    String Content,Subject;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_practicedisplay);
+        Subject = "";
+        Bundle extras = getIntent().getExtras();
+        Subject = extras.getString("subject");
 		 tv1=(TextView)findViewById(R.id.tv);
 	        btnNext=(Button)findViewById(R.id.btn);
 	        rg=(RadioGroup)findViewById(R.id.radioGroup1);
@@ -37,45 +58,147 @@ public class Practicedisplay extends Activity {
 	        rb2=(RadioButton) findViewById(R.id.radio2);
 	        rb3= (RadioButton) findViewById(R.id.radio3);
 	        
-	        
-	        tv1.setText(questions[flag]);
-	       rb0.setText(opt[0]);
-	        rb1.setText(opt[1]);
-	        rb2.setText(opt[2]);
-	        rb3.setText(opt[3]);
+	    new LongOperation().execute("http://amritaplacementtrainer.comlu.com/questions.php");
+
 	        setupmessagebutton();
 	    }
 	        
-	        private void setupmessagebutton()
-	    	{
-	    		
-	    			Button messageButton5=(Button)findViewById(R.id.btn);
-	    		messageButton5.setOnClickListener(new View.OnClickListener(){
-	    			public void onClick(View arg0){
-	    				
-	    			
-	    	for(i=0;i<4;i++)	
-	    	{
-	        
-	        	tv1.setText(questions[flag*1]);
-	        	rb0.setText(opt[flag*4]);
-	        	rb1.setText(opt[(flag*4)+1]);
-	        	rb2.setText(opt[(flag*4)+2]);
-	        	rb3.setText(opt[(flag*4)+3]);
-	        	
-	        
-	    	}
-	    	
-	    	flag++;
-	    	if(flag>5)
-			{
-		Toast.makeText(getApplicationContext(),"YOU HAVE COME TO THE END",Toast.LENGTH_SHORT).show();
-			}
-	    			}
-	    			
-	    				
-	    		});
-	    	}
+        private void setupmessagebutton()
+        {
+            Button messageButton5=(Button)findViewById(R.id.btn);
+            messageButton5.setOnClickListener(new View.OnClickListener(){
+                public void onClick(View arg0){
+                    if(flag < ques.length)
+                    {
+                        tv1.setText(ques[i].que);
+                        rb0.setText(ques[i].opt[0]);
+                        rb1.setText(ques[i].opt[1]);
+                        rb2.setText(ques[i].opt[2]);
+                        rb3.setText(ques[i].opt[3]);
+                    }
+                    else {
+                        Toast.makeText(Practicedisplay.this,"Test is complete.",Toast.LENGTH_SHORT).show();
+                        //TODO send answers to the server
+                        finish();
+                    }
+                    flag++;
+                    i++;
+                }
+            });
+        }
 
+
+    // Class with extends AsyncTask class
+    private class LongOperation  extends AsyncTask<String, Void, Void> {
+
+        private final HttpClient Client = new DefaultHttpClient();
+        private String Error = null;
+        private ProgressDialog Dialog = new ProgressDialog(Practicedisplay.this);
+
+        protected void onPreExecute() {
+            // NOTE: You can call UI Element here.
+
+            //UI Element
+            Dialog.setMessage("Downloading Questions..");
+            Dialog.show();
+        }
+
+        // Call after onPreExecute method
+        protected Void doInBackground(String... urls) {
+            try {
+
+                // Call long running operations here (perform background computation)
+                // NOTE: Don't call UI Element here.
+
+                // Server url call by GET method
+                HttpPost httpPost = new HttpPost(urls[0]);
+                List<NameValuePair> postData = new ArrayList<NameValuePair>(1);
+                postData.add(new BasicNameValuePair("subject",Subject));
+                httpPost.setEntity(new UrlEncodedFormEntity(postData));
+                HttpResponse response = Client.execute(httpPost);
+                Content = EntityUtils.toString(response.getEntity());
+                Log.d("Content", Content);
+                // Decoding the json data
+
+
+            } catch (ClientProtocolException e) {
+                Error = e.getMessage();
+                cancel(true);
+            } catch (IOException e) {
+                Error = e.getMessage();
+                cancel(true);
+            }
+
+            return null;
+        }
+
+        protected void onPostExecute(Void unused) {
+            // NOTE: You can call UI Element here.
+            jsonDecode();
+            // Close progress dialog
+            Dialog.dismiss();
+            // Setting the first question
+            tv1.setText(ques[0].que);
+            rb0.setText(ques[0].opt[0]);
+            rb1.setText(ques[0].opt[1]);
+            rb2.setText(ques[0].opt[2]);
+            rb3.setText(ques[0].opt[3]);
+        }
+
+
+    }
+
+    public void jsonDecode(){
+
+        try {
+            Log.d("Inside json decode","");
+            /****** Creates a new JSONObject with name/value mappings from the JSON string. ********/
+        if(Content.equals("")) {
+            JSONObject jsonResponse = new JSONObject(Content);
+            /***** Returns the value mapped by name if it exists and is a JSONArray. ***/
+            /*******  Returns null otherwise.  *******/
+            JSONArray jsonMainNode = jsonResponse.optJSONArray("Questions");
+            /*********** Process each JSON Node ************/
+            int lengthJsonArr = jsonMainNode.length();
+            ques = new Question[lengthJsonArr];
+
+            for (int i = 0; i < lengthJsonArr; i++) {
+                /****** Get Object for each JSON node.***********/
+                JSONObject jsonChildNode = jsonMainNode.getJSONObject(i);
+                Log.d("child node",jsonChildNode.toString(1));
+                /******* Fetch node values **********/
+                ques[i] = new Question();
+                ques[i].id = Integer.parseInt(jsonChildNode.optString("id_no"));
+                if(jsonChildNode.get("question").equals(null))
+                    ques[i].que = "";
+                else
+                    ques[i].que = jsonChildNode.optString("question");
+                String[] opt = new String[4];
+                if(jsonChildNode.get("optionA").equals(null))
+                    opt[0] = "";
+                else
+                    opt[0] = jsonChildNode.get("optionA").toString();
+                if(jsonChildNode.get("optionB").equals(null))
+                    opt[1] = "";
+                else
+                    opt[1] = jsonChildNode.get("optionB").toString();
+                if(jsonChildNode.get("optionC").equals(null))
+                    opt[2] = "";
+                else
+                    opt[2] = jsonChildNode.get("optionC").toString();
+                if(jsonChildNode.get("optionD").equals(null))
+                    opt[3] = "";
+                else
+                    opt[3] = jsonChildNode.optString("optionD");
+                ques[i].setOpt(opt);
+            }
+        }
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Toast.makeText(this,"Something went wrong try again later.",Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
 
 }
--- src/com/example/amritaplacementtrainer/Techprac.java
+++ src/com/example/amritaplacementtrainer/Techprac.java
@@ -1,7 +1,11 @@
 package com.example.amritaplacementtrainer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+
 public class Techprac extends Activity {
 
 	@Override
@@ -10,6 +14,13 @@ public class Techprac extends Activity {
 		setContentView(R.layout.activity_techprac);
 	}
 
+    public void clicking(View v){
+        Button b = (Button)v;
+        Intent intent = new Intent(this,Practicedisplay.class);
+        intent.putExtra("subject",b.getText());
+        startActivity(intent);
+        finish();
+    }
 
 
 }
