b5fbf2d87172528fb38c9cd1807e6ab586670b88
sundarkid <sundareswaran25@gmail.com>
2015-03-24 16:43:52 +0530

    Comitting the changes.

---
+++ src/com/example/amritaplacementtrainer/Question.java
@@ -0,0 +1,17 @@
+package com.example.amritaplacementtrainer;
+
+public class Question {
+    public int id;
+	public String que;
+	public String[] opt;
+    public String ans;
+
+    public void setOpt(String[] opt) {
+        this.opt = opt;
+    }
+}
+
+	
+	
+
+
